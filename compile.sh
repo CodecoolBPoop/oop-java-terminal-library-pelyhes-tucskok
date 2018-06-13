7a09ddfe6f8640a30f0d1342a2e355b69901d226
Tibor Maron <tibormaron@gmail.com>
2018-06-13 09:28:51 +0200

    modified compile.sh to run program after compilation

--- compile.sh
+++ compile.sh
@@ -2,3 +2,4 @@
 javac src/com/codecool/termlib/*.java
 jar cf termlib.jar src/com/codecool/termlib/*.class
 javac -cp termlib.jar src/com/codecool/hangman/*.java
+java -cp src com.codecool.hangman.Hangman
